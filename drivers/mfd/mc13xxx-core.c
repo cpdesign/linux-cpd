186cff5ba48d33821e2ad1473932da9adcac2ddb
Marc Reilly <marc@cpdesign.com.au>
2011-05-12 12:35:59 +1000

    mc13xxx-core: ack any existing interrupt before requesting, not after
    Signed-off-by: Marc Reilly <marc@cpdesign.com.au>

--- drivers/mfd/mc13xxx-core.c
+++ drivers/mfd/mc13xxx-core.c
@@ -633,9 +633,9 @@ int mc13xxx_adc_do_conversion(struct mc13xxx *mc13xxx, unsigned int mode,
 	}
 
 	dev_dbg(mc13xxx->dev, "%s: request irq\n", __func__);
+	mc13xxx_irq_ack(mc13xxx, MC13XXX_IRQ_ADCDONE);
 	mc13xxx_irq_request(mc13xxx, MC13XXX_IRQ_ADCDONE,
 			mc13xxx_handler_adcdone, __func__, &adcdone_data);
-	mc13xxx_irq_ack(mc13xxx, MC13XXX_IRQ_ADCDONE);
 
 	mc13xxx_reg_write(mc13xxx, MC13XXX_ADC0, adc0);
 	mc13xxx_reg_write(mc13xxx, MC13XXX_ADC1, adc1);
